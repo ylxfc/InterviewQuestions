009c472b0a0f5648cd4572a4b46a24498cdfc0c3
ylxfc <yulixfc@gmail.com>
2014-03-11 16:37:58 +0800

    CVTE面试练习

---
+++ 常见面试题练习/src/com/yuli/mianshi/esr.java
@@ -0,0 +1,18 @@
+package com.yuli.mianshi;
+
+public class esr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int i = new int;
+		int i = 12;
+		int k = 25;
+		i <<= 2;
+		String t = null;
+		String t1 = "60.0";
+		int y = 2;
+		int z = (y++)*3;
+		System.out.println(t.toString());
+	}
+
+}
